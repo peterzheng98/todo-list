30c2b383e61c02902318db7e0ddbd6dba22fe118
Peter Zheng <peterzheng98@sjtu.edu.cn>
2020-03-03 23:54:05 +0800

    add(frontend): add detail frame

--- app.py
+++ app.py
@@ -7,12 +7,21 @@ app.config["SECRET_KEY"] = 'pikachu-loves-watermelon'
 overall_list = []
 
 
+@app.route('/detail/<int:ident>')
+def disp_detail(ident):
+    sublist = overall_list[ident]
+    return render_template('detail.html',
+                           table_content=[('Title', sublist[0]), ('Context', sublist[1]), ('Add Time', sublist[2])],
+                           overall_id=ident)
+
+
 @app.route('/')
 def hello_world():
     header_content = ['#', 'Title', 'Context', 'AddTime', 'Detail']
     table_content = []
     for idx, d in enumerate(overall_list):
-        table_content.append([('', '{}'.format(idx)), ('', d[0]), ('', d[1]), ('', d[2]), ('/detail/{}'.format(idx), 'Go>')])
+        table_content.append(
+            [('', '{}'.format(idx)), ('', d[0]), ('', d[1]), ('', d[2]), ('/detail/{}'.format(idx), 'Go>')])
     return render_template('index.html',
                            day_string=time.strftime('%Y-%m-%d', time.localtime(time.time())),
                            table_header=header_content,
